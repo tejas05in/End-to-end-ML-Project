5b19b416deef58e2cb4f6ca3b0421578e63a28e3
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2024-01-03 19:00:55 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info("My name is Tejas")
+STAGE_NAME = 'Data Ingestion Stage'
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
