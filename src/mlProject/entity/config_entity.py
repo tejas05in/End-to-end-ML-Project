9da98e8f0cbeba15b0f1f5ad03d811f8f03b1ec6
Dr. TEJAS. J <tejas05in@yahoo.co.in>
2024-01-03 19:55:26 +0530

    data transformation completed

--- src/mlProject/entity/config_entity.py
+++ src/mlProject/entity/config_entity.py
@@ -15,3 +15,12 @@ class DataValidationConfig:
     STATUS_FILE: str
     unzip_data_dir : Path
     all_schema: dict
+
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
